05ff3c97bde9f168306f2e6fc2382ec20f895a0d
nikolaskalvin <nikolaskalvin@gmail.com>
2024-08-09 18:14:30 +0700

    first commit

---
+++ TBO_TUGAS 1.A PYTHON KALVIN.py
@@ -0,0 +1,15 @@
+#.CODING PYTHON - [ TUGAS - 1.A ]
+#.KULIAH TEORI BAHASA & OTOMATA
+#.DOSEN = Arnes Y.V, M.Kom
+
+print("NAMA = NIKOLAS KALVIN")
+print("NPM = 21421002")
+print('')
+# Memasukkan Inputan Angka
+angka = float(input('Tulisan Angka: '))
+
+# Menghitung Akar Kuadrat
+akar_kuadrat = angka ** 0.5
+
+# Menampilkan Hasil Akar Kuadrat
+print('Akar Kuadrat dari %0.3f adalah % 0.3f'% (angka, akar_kuadrat))
